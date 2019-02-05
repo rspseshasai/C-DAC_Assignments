db447bf768ecfe0bbbf7846a4ecb876ad555288b
Pavan SESHA sai <rspseshasai@gmail.com>
2019-02-05 19:40:01 +0530

    ReverseOfaNumber added

--- C_Assignment-2/ReverseNumber/ReverseNumber.cpp
+++ C_Assignment-2/ReverseNumber/ReverseNumber.cpp
@@ -5,5 +5,17 @@ using namespace std;
 
 int main()
 {
-    
+	int n;
+	cout << "Enter a number\n";
+	cin >> n;
+	int ans=0, t=1;
+	while (n > 0)
+	{
+		ans = (ans * t) + (n % 10);
+		//cout << ans << " \n";
+		t = 10;
+		n /= 10;
+	}
+	cout << ans << "\n";
+	system("pause");
 }
