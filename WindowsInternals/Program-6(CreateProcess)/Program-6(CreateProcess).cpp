006bc35375b4fa2d7f1c66742d868c0d4c7700ea
Pavan SESHA sai <rspseshasai@gmail.com>
2019-03-01 07:14:20 +0530

    Processes

--- WindowsInternals/Program-6(CreateProcess)/Program-6(CreateProcess).cpp
+++ WindowsInternals/Program-6(CreateProcess)/Program-6(CreateProcess).cpp
@@ -4,10 +4,22 @@
 #define MAX 100
 
 using namespace std;
+STARTUPINFO startInfo;
+PROCESS_INFORMATION processInfo;
+
+
 
 int main()
 {
-	
+	ZeroMemory(&startInfo, sizeof(startInfo));
+	ZeroMemory(&processInfo, sizeof(processInfo));
+	int retValue = CreateProcess(TEXT("C:/Users/rspse/Downloads/Winobj.exe"),
+		NULL, NULL, NULL, TRUE, 0, NULL, NULL, &startInfo, &processInfo
+		);
+
+	if (0 == retValue)
+		cout << "Process not Created "<<GetLastError()<<"\n";
+
 	system("pause");
 	return 0;
 }
---
+++ WindowsInternals/Program-7(Create2ChildProcesses)/Program-7(Create2ChildProcesses).cpp
@@ -0,0 +1,62 @@
+#include "pch.h"
+#include<Windows.h>
+#include <iostream>
+#define MAX 100
+
+using namespace std;
+STARTUPINFO si1, si2;
+PROCESS_INFORMATION pi1, pi2;
+
+
+
+int main(int argc, TCHAR *argv[])
+{
+	ZeroMemory(&si1, sizeof(si1));
+	si1.cb = sizeof(si1);
+	ZeroMemory(&pi1, sizeof(pi1));
+
+	ZeroMemory(&si2, sizeof(si2));
+	si2.cb = sizeof(si2);
+	ZeroMemory(&pi2, sizeof(pi2));
+
+	if (!CreateProcess(TEXT("C:/Users/rspse/Downloads/Winobj.exe"),
+		NULL,
+		NULL,           
+		NULL,           
+		FALSE,          
+		0,              
+		NULL,           
+		NULL,           
+		&si1,            
+		&pi1)           
+		) {
+		printf("CreateProcess failed (%d).\n", GetLastError());
+		return 0;
+	}
+
+	if (!CreateProcess(TEXT("C:/Users/rspse/Downloads/Winobj.exe"),
+		NULL,
+		NULL,
+		NULL,
+		FALSE,
+		0,
+		NULL,
+		NULL,
+		&si2,
+		&pi2)
+		) {
+		printf("CreateProcess failed (%d).\n", GetLastError());
+		return 0;
+	}
+
+	DWORD Ids[2];
+
+	Ids[0] = pi1.dwProcessId;
+	Ids[1] = pi2.dwProcessId;
+
+	for (int i = 0; i < 2; i++)
+		cout << "Process "<<i<<" Id is : " << Ids[i]<<"\n";
+	system("pause");
+	return 0;
+}
+
